43b36243d3de2fddaf37616c27bda3c5ee71bf04
BaileyFirman <bailey.firman@nintex.com>
2020-11-01 23:12:45 +1300

    4.2.2 Added Token type in Token module

---
+++ Token.fs
@@ -0,0 +1,14 @@
+namespace LoxFs
+
+open TokenType
+
+module Token =
+    type Token(tokenType, lexeme, literal, line) =
+        let tokenType: TokenType = tokenType
+        let lexeme: string = lexeme
+        let literal: obj = literal
+        let line: int = line
+        member this.ToString() =
+            let tokenTypeString = tokenType.ToString()
+            let literalString = literal.ToString()
+            printfn "%s %s %s" tokenTypeString lexeme literalString
