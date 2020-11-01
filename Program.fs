79a30c5fb62d91d8c5c3f6a60b5017fe23d011dd
BaileyFirman <bailey.firman@nintex.com>
2020-11-01 22:59:04 +1300

    4.2.1 Added main function to LoxFs module

--- Program.fs
+++ Program.fs
@@ -1,33 +1,40 @@
+namespace LoxFs
+
 open System
 open System.IO
 
+module LoxFs =
+    [<EntryPoint>]
+    let main argv =
+        let mutable hadError = false
 
-[<EntryPoint>]
-let main argv =
-    let report line where message =
-        printfn "[line %s] Error%s:%s" line where message
+        let report line where message =
+            printfn "[line %s] Error%s:%s" line where message
 
-    let error line message =
-        report line "" message
+        let error line message = report line "" message
 
-    let usage code =
-        printfn "Usage: dotnet run [script]"
-        Environment.Exit code
-    
-    let runFile path =
-        let file = File.ReadAllText path
-        ()
+        let usage code =
+            printfn "Usage: dotnet run [script]"
+            Environment.Exit code
 
-    let rec runPrompt code =
-        printf "> "
-        let line = Console.ReadLine()
-        printfn "%s" line
-        runPrompt code
+        let runFile path =
+            let file = File.ReadAllText path
+            match hadError with
+            | true -> Environment.Exit 65
+            | false -> Environment.Exit 64
+            ()
 
-    let exitCode = 64
+        let rec runPrompt code =
+            printf "> "
+            let line = Console.ReadLine()
+            printfn "%s" line
+            hadError <- false
+            runPrompt code
 
-    match argv.Length with
-    | 3 -> usage exitCode
-    | 2 -> runFile argv.[0]
-    | _ -> runPrompt exitCode
-    0 // return an integer exit code
+        let exitCode = 64
+
+        match argv.Length with
+        | 3 -> usage exitCode
+        | 2 -> runFile argv.[0]
+        | _ -> runPrompt exitCode
+        0 // return an integer exit code
