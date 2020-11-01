f4f5540fa8b040b5bf366939dafec81002060619
BaileyFirman <bailey.firman@nintex.com>
2020-11-01 22:59:23 +1300

    4.2.1 Added TokenType union type

---
+++ TokenType.fs
@@ -0,0 +1,47 @@
+namespace LoxFs
+
+module TokenType =
+    type TokenType =
+        // Single-character tokens.
+        | LEFTPAREN
+        | RIGHTPAREN
+        | LEFTBRACE
+        | RIGHTBRACE
+        | COMMA
+        | DOT
+        | MINUS
+        | PLUS
+        | SEMICOLON
+        | SLASH
+        | STAR
+        // One or two character tokens.
+        | BANG
+        | BANGEQUAL
+        | EQUAL
+        | EQUALEQUAL
+        | GREATER
+        | GREATEREQUAL
+        | LESS
+        | LESSEQUAL
+        // Literals.
+        | IDENTIFIER
+        | STRING
+        | NUMBER
+        // Keywords
+        | AND
+        | CLASS
+        | ELSE
+        | FALSE
+        | FUN
+        | FOR
+        | IF
+        | NIL
+        | OR
+        | PRINT
+        | RETURN
+        | SUPER
+        | THIS
+        | TRUE
+        | VAR
+        | WHILE
+        | EOF
