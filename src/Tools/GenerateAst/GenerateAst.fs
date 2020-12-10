5f954dcb2daf6c03e43263fd9f3b0155de964a88
BaileyFirman <bailey.firman@nintex.com>
2020-12-10 18:16:41 +1300

    Removed hidden chars from GenerateAst.fsproj, added initial defineAst output

--- src/Tools/GenerateAst/GenerateAst.fs
+++ src/Tools/GenerateAst/GenerateAst.fs
@@ -1,4 +1,5 @@
 open System
+open System.Text
 
 let exitWithMessage message =
     printfn "%s" message
@@ -6,10 +7,29 @@ let exitWithMessage message =
 
 let defineAst outputDir baseName types =
     let path = $"{outputDir}/{baseName}/.java"
+
+    let program =
+        [|
+            "package com.craftinginterpreters.lox;"
+            "\n"
+            "import java.util.List;"
+            "\n"
+            "abstract class "
+            baseName
+            " {"
+            "}"
+            "\n"
+        |]
+        |> String.Concat
+
+    printf "%s" program 
     0
 
 [<EntryPoint>]
 let main argv =
     let outputDir = if argv.Length <> 1 then String.Empty else argv.[0]
-    if outputDir = String.Empty then exitWithMessage "Usage: generate_ast <output directory>" else ()
+    defineAst outputDir "TEST" () |> ignore
+    if outputDir = String.Empty
+        then exitWithMessage "Usage: generate_ast <output directory>"
+        else ()
     0
