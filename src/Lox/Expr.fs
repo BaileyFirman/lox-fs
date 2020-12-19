0528330cd1628fdea6a8df86d45517b1f14d8be1
BaileyFirman <bailey.firman@nintex.com>
2020-12-19 14:57:30 +1300

    5.3.2: Added initial reference version of Expr.fs

--- src/Lox/Expr.fs
+++ src/Lox/Expr.fs
@@ -1,13 +1,37 @@
 namespace LoxFs
 
 open Token
-open TokenType
 
 module Expr =
-    type Expr() = class end
+    type IVisitor<'T> =
+        abstract VisitBinaryExpr: Binary -> 'T
+        abstract VisitGroupingExpr: Grouping -> 'T
+        abstract VisitLiteralExpr: Literal -> 'T
+        abstract VisitUnaryExpr: Unary -> 'T
 
-    type Binary(left, operator, right)=
+    and Expr() =
+        // abstract Accept: IVisitor<'T> -> 'T
+        member __.Accept (visitor: IVisitor<'T>) = visitor
+
+    and Binary(left, operator, right) as this =
         inherit Expr()
         let left: Expr = left
         let literal: Token = operator
         let right: Expr = right
+        member __.Accept (visitor: IVisitor<'T>) = visitor.VisitBinaryExpr(this)
+
+    and Grouping(expression) as this =
+        inherit Expr()
+        let expression: Expr = expression
+        member __.Accept (visitor: IVisitor<'T>) = visitor.VisitGroupingExpr(this)
+
+    and Literal(value) as this =
+        inherit Expr()
+        let value: Expr = value
+        member __.Accept (visitor: IVisitor<'T>) = visitor.VisitLiteralExpr(this)
+
+    and Unary(operator, right) as this =
+        inherit Expr()
+        let operator: Token = operator
+        let right: Expr = right
+        member __.Accept (visitor: IVisitor<'T>) = visitor.VisitUnaryExpr(this)
