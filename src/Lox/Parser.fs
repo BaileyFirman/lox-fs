712e3df03febeab5ef9244c462e16493e624e55e
BaileyFirman <bailey.firman@nintex.com>
2020-12-20 21:18:32 +1300

    6.2.1: Initial matchToken functions in Parser

--- src/Lox/Parser.fs
+++ src/Lox/Parser.fs
@@ -1,8 +1,27 @@
 namespace LoxFs
 
 open Token
+open TokenType
 
 module Parser =
     type Parser(tokens) =
         let tokens: Token[] = tokens
         let mut current = 0
+
+        let rec matchToken (tokens: TokenType[]): bool =
+            let head = tokens.[0]
+            match check head with
+            | true ->
+                advance ()
+                true
+            | false -> matchToken tokens.[1..]
+            |> ignore
+            false
+
+        let equality (): Expr =
+            let expr = comparison()
+
+        let rec matchEquality () =
+            match BANGEQUAL
+
+        member __.Expression = equality()
