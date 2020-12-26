4d147fb9e32d47042122881619ab892d1b5a18ef
BaileyFirman <bailey.firman@nintex.com>
2020-12-26 23:50:28 +1300

    6.2.1: Added peek, previous, isAtEnd, and check functions to Parser

--- src/Lox/Parser.fs
+++ src/Lox/Parser.fs
@@ -6,13 +6,34 @@ open TokenType
 module Parser =
     type Parser(tokens) =
         let tokens: Token[] = tokens
-        let mut current = 0
+        let mutable current = 0
+
+        let peek () =
+            tokens.[current]
+
+        let previous () =
+            tokens.[current - 1]
+
+        let isAtEnd () =
+            peek().tokenType = EOF
+
+        let advance () =
+            let atEnd = isAtEnd ()
+            if not atEnd
+            then current <- current + 1
+            else ()
+            previous ()
+
+        let check tokenType =
+            if isAtEnd ()
+            then false
+            else peek().tokenType = tokenType
 
         let rec matchToken (tokens: TokenType[]): bool =
             let head = tokens.[0]
             match check head with
             | true ->
-                advance ()
+                advance () |> ignore
                 true
             | false -> matchToken tokens.[1..]
             |> ignore
