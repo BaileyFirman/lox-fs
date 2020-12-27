dc1372f1c173e28e680df9ab01e98a74f8dd9366
BaileyFirman <bailey.firman@nintex.com>
2020-12-27 21:12:51 +1300

    6.2.1: Introduced term and comparison parser functions, added stub for factor function

--- src/Lox/Parser.fs
+++ src/Lox/Parser.fs
@@ -2,34 +2,28 @@ namespace LoxFs
 
 open Token
 open TokenType
+open Expr
 
 module Parser =
     type Parser(tokens) =
-        let tokens: Token[] = tokens
+        let tokens: Token [] = tokens
         let mutable current = 0
 
-        let peek () =
-            tokens.[current]
+        let peek () = tokens.[current]
 
-        let previous () =
-            tokens.[current - 1]
+        let previous () = tokens.[current - 1]
 
-        let isAtEnd () =
-            peek().tokenType = EOF
+        let isAtEnd () = peek().tokenType = EOF
 
         let advance () =
             let atEnd = isAtEnd ()
-            if not atEnd
-            then current <- current + 1
-            else ()
+            if not atEnd then current <- current + 1 else ()
             previous ()
 
         let check tokenType =
-            if isAtEnd ()
-            then false
-            else peek().tokenType = tokenType
+            if isAtEnd () then false else peek().tokenType = tokenType
 
-        let rec matchToken (tokens: TokenType[]): bool =
+        let rec matchToken (tokens: TokenType []): bool =
             let head = tokens.[0]
             match check head with
             | true ->
@@ -39,10 +33,42 @@ module Parser =
             |> ignore
             false
 
-        let equality (): Expr =
-            let expr = comparison()
+        let factor (): IExpr = Literal("FACTOR") :> IExpr
 
-        let rec matchEquality () =
-            match BANGEQUAL
+        let term (): IExpr =
+            let mutable expr = factor ()
+            let matchTokens = [| MINUS; PLUS |]
 
-        member __.Expression = equality()
+            let rec innerTerm (): unit =
+                match matchToken matchTokens with
+                | true ->
+                    let operator = previous ()
+                    let right = factor ()
+                    expr <- Binary(expr, operator, right)
+                    innerTerm ()
+                | false -> ()
+
+            innerTerm ()
+            expr
+
+
+        let comparison (): IExpr =
+            let mutable expr = term ()
+
+            let matchTokens =
+                [| GREATER
+                   GREATEREQUAL
+                   LESS
+                   LESSEQUAL |]
+
+            let rec innerComparison (): unit =
+                match matchToken matchTokens with
+                | true ->
+                    let operator = previous ()
+                    let right = term ()
+                    expr <- Binary(expr, operator, right)
+                    innerComparison ()
+                | false -> ()
+
+            innerComparison ()
+            expr
