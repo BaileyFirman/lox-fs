bb63b4fe6f51e69a3607bf3a967eb2efa2ad102e
BaileyFirman <bailey.firman@nintex.com>
2021-05-03 17:25:06 +1200

    Add initial Stmt file

---
+++ src/Lox/Stmt.fs
@@ -0,0 +1,24 @@
+namespace LoxFs
+
+open Token
+open Expr
+
+module Stmt =
+    type IStmtVisitor<'T> =
+        abstract VisitExpressionStmt : Expression -> 'T
+        abstract VisitPrintStmt : Print -> 'T
+
+    and IStmt =
+        abstract Accept : IStmtVisitor<'T> -> 'T
+
+    and Expression(expression) as this =
+        member __.Expression : IExpr = expression
+
+        interface IStmt with
+            member __.Accept(visitor: IStmtVisitor<'T>) = visitor.VisitExpressionStmt(this)
+
+    and Print(expression) as this =
+        member __.Expression : IExpr = expression
+
+        interface IStmt with
+            member __.Accept(visitor: IStmtVisitor<'T>) = visitor.VisitPrintStmt(this)
