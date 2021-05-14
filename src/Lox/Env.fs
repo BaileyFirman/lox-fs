60306e2ced495f1e73577ec6e3b965a54bae7e88
BaileyFirman <bailey.firman@nintex.com>
2021-05-14 23:32:08 +1200

    Added Env module wrap a nested Env Dict

---
+++ src/Lox/Env.fs
@@ -0,0 +1,36 @@
+namespace LoxFs
+
+open Microsoft.FSharp.Core
+open System
+open System.Collections.Generic
+open Token
+
+module Env =
+    type Env(enclosingEnv: Env option) =
+        let mutable values = new Dictionary<string, obj>()
+
+        let mutable enclosing: Env option = enclosingEnv
+
+        member public __.Get (name: Token) =
+            match values.ContainsKey name.lexeme with
+            | true -> values.[name.lexeme]
+            | false -> 
+                match enclosing with
+                | Some e -> e.Get name
+                | None -> new obj() // Unreachable or error
+
+        member public __.Define (name: string) value =
+            values.[name] <- value
+
+        member public __.Assign (name: Token, value: obj) =
+            match values.ContainsKey name.lexeme with
+            | true -> values.[name.lexeme] <- value
+            | false -> ()
+
+        member public __.Assign (name: Token, value: obj option) =
+            let actual =
+                match value with
+                | Some v -> value.Value
+                | None -> new obj() // should be unreachable
+
+            __.Assign (name, actual)
