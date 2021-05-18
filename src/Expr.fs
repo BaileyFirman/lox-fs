f993f64b9b2d32ff959fcd84aa010c302879988a
BaileyFirman <bailey.firman@nintex.com>
2021-05-18 22:32:06 +1200

    Simplify IExpr type construction

--- src/Expr.fs
+++ src/Expr.fs
@@ -4,71 +4,70 @@ open Token
 
 module Expr =
     type IVisitor<'T> =
+        abstract VisitAssignExpr : Assign -> 'T
         abstract VisitBinaryExpr : Binary -> 'T
+        abstract VisitCallExpr : Call -> 'T
         abstract VisitGroupingExpr : Grouping -> 'T
         abstract VisitLiteralExpr : Literal -> 'T
+        abstract VisitLogicalExpr : Logical -> 'T
         abstract VisitUnaryExpr : Unary -> 'T
         abstract VisitVariableExpr : Variable -> 'T
-        abstract VisitAssignExpr : Assign -> 'T
-        abstract VisitLogicalExpr : Logical -> 'T
-        abstract VisitCallExpr : Call -> 'T
 
     and IExpr =
         abstract Accept : IVisitor<'T> -> 'T
 
-    and Binary(left, operator, right) as this =
-        member __.Left : IExpr = left
-        member __.Operator : Token = operator
-        member __.Right : IExpr = right
+    and Assign(name: Token, value: IExpr) =
+        member __.Name = name
+        member __.Value = value
 
         interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitBinaryExpr(this)
+            member __.Accept v = v.VisitAssignExpr __
 
-    and Grouping(expression) as this =
-        member __.Expression : IExpr = expression
-
-        interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitGroupingExpr(this)
-
-    and Literal(value: obj) as this =
-        member __.Value : obj = value
+    and Binary(left: IExpr, operator: Token, right: IExpr) =
+        member __.Left = left
+        member __.Operator = operator
+        member __.Right = right
 
         interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitLiteralExpr(this)
+            member __.Accept v = v.VisitBinaryExpr __
 
-    and Unary(operator, right) as this =
-        member __.Operator : Token = operator
-        member __.Right : IExpr = right
-
-        interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitUnaryExpr(this)
-
-    and Variable(name) as this =
-        member __.Name : Token = name
+    and Call(callee: IExpr, paren: Token, arguments: list<IExpr>) =
+        member __.Callee = callee
+        member __.Paren = paren
+        member __.Argurments = arguments
 
         interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitVariableExpr(this)
+            member __.Accept v = v.VisitCallExpr __
 
-    and Assign(name, value) as this =
-        member __.Name : Token = name
-        member __.Value : IExpr = value
-
-        interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitAssignExpr(this)
-
-    and Logical(left, operator, right) as this =
-        member __.Left : IExpr = left
-        member __.Operator : Token = operator
-        member __.Right : IExpr = right
+    and Grouping(expression: IExpr) =
+        member __.Expression = expression
 
         interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) = visitor.VisitLogicalExpr(this)
+            member __.Accept v = v.VisitGroupingExpr __
 
-    and Call(callee, paren, arguments) as this =
-        member __.Callee : IExpr = callee
-        member __.Paren : Token = paren
-        member __.Argurments : list<IExpr> = arguments
+    and Literal(value: obj) =
+        member __.Value = value
 
         interface IExpr with
-            member __.Accept(visitor: IVisitor<'T>) =
-                visitor.VisitCallExpr(this)
+            member __.Accept v = v.VisitLiteralExpr __
+
+    and Logical(left: IExpr, operator: Token, right: IExpr) =
+        member __.Left = left
+        member __.Operator = operator
+        member __.Right = right
+
+        interface IExpr with
+            member __.Accept v = v.VisitLogicalExpr __
+
+    and Unary(operator: Token, right: IExpr) =
+        member __.Operator = operator
+        member __.Right = right
+
+        interface IExpr with
+            member __.Accept v = v.VisitUnaryExpr __
+
+    and Variable(name: Token) =
+        member __.Name = name
+
+        interface IExpr with
+            member __.Accept v = v.VisitVariableExpr __
